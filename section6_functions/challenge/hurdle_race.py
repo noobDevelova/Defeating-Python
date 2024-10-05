23764445191d503b3a3da285a284549ec98b0de9
noobDevelova <randyrisdiansyah@gmail.com>
2024-10-05 22:27:55 +0700

    Function and Hangman Challenge

---
+++ section6_functions/challenge/hurdle_race.py
@@ -0,0 +1,18 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def walk():
+#     if(wall_on_right() and front_is_clear()):
+#         move()
+#     elif(right_is_clear()):
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#     else:
+#         turn_left()
+    
+# while at_goal() != True:
+#     walk()
