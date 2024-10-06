e451ee737d3dc223625ec85d03554f0bec5f0273
noobDevelova <randyrisdiansyah@gmail.com>
2024-10-06 08:00:20 +0700

    New section: dictionary

---
+++ section8_dictionary/nesting_disctionary/main.py
@@ -0,0 +1,23 @@
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": [1,2,3,4,5,6],
+    "d": {
+        1: 2,
+        3: 4
+    },
+    "f": [
+        {
+            "name": 'Randi'
+        }
+    ]
+}
+
+
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
+
+print(starting_dictionary)
